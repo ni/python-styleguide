0c27da0518f38530998773445289e6b064b2b1a3
Joshua Cannon <joshua.cannon@ni.com>
2020-08-11 14:49:33 -0500

    Add subsection rule test

--- tests/test_convention_doc/test_subsections.py
+++ tests/test_convention_doc/test_subsections.py
@@ -12,3 +12,12 @@ def test_subsection_identifiers_strictly_increasing(section):
     for index, subsection in enumerate(section.subsections):
         assert subsection.identifier.split(".")[-1] == str(index + 1)
 
+
+def test_subsection_isnt_rule(subsection):
+    assert not (
+        subsection.header_text.startswith(" ✔️ **DO**")
+        or subsection.header_text.startswith(" ✔️ **CONSIDER**")
+        or subsection.header_text.startswith(" ❌ **AVOID**")
+        or subsection.header_text.startswith(" ❌ **DO NOT**")
+    )
+
