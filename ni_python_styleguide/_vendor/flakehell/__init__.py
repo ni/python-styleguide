b2c47591580ce3e8194945ef9e7832f8e744c616
Joshua Cannon <joshua.cannon@ni.com>
2020-09-22 13:16:52 -0500

    Remove flakehell code

--- ni_python_styleguide/_vendor/flakehell/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Flake8 wrapper to make it nice and configurable
-"""
-
-# app
-from ._cli import entrypoint, flake8_entrypoint
-from ._version import __version__
-
-
-__all__ = ['entrypoint', 'flake8_entrypoint', '__version__']
