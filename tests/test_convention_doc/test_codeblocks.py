ad2c64d0a0e2860335a3bdc37e257cab60c9aabc
Joshua Cannon <joshua.cannon@ni.com>
2020-09-25 09:51:08 -0500

    Fix tests by adding format param (#28)

--- tests/test_convention_doc/test_codeblocks.py
+++ tests/test_convention_doc/test_codeblocks.py
@@ -8,7 +8,7 @@ def lint_codeblock(styleguide, tmp_path):
     def run_linter(codeblock, *styleguide_args):
         test_file = tmp_path / "test.py"
         test_file.write_text(codeblock.contents, encoding="utf-8")
-        return styleguide("lint", test_file, *styleguide_args)
+        return styleguide("lint", *styleguide_args, test_file)
 
     return run_linter
 
@@ -21,29 +21,27 @@ def test_rule_codeblocks_documents_bad_good_best(codeblock):
     assert codeblock.descriptor in ("Good", "Bad", "Best")
 
 
-def test_bad_codeblocks_document_lint_errors(lint_codeblock, bad_codeblock, capsys):
+def test_bad_codeblocks_document_lint_errors(lint_codeblock, bad_codeblock):
     if bad_codeblock.rule.is_automatically_enforced:
-        assert not lint_codeblock(
-            bad_codeblock, "--format", "json"
-        ), 'Expected automatically enforced "bad" codeblock to cause lint error'
+        result = lint_codeblock(bad_codeblock, "--format='%(code)s'")
+        assert not result, result.output
 
-        error_codes = set(
-            json.loads(lint_error)["code"] for lint_error in capsys.readouterr().out.splitlines()
-        )
-
+        error_codes = set(code.strip("'") for code in result.output.splitlines())
+        assert error_codes
         assert error_codes.issubset(
             set(bad_codeblock.rule.error_codes)
         ), '"Bad" codeblock caused unexpected lint error'
     else:
-        # You can check stdout from the test results to see the lint errors
-        assert lint_codeblock(
-            bad_codeblock
-        ), '"Bad" codeblock caused a lint error, but isn\'t marked as automatically enforced'
+        # "Bad" codeblock caused a lint error, but isn't marked as automatically enforced
+        result = lint_codeblock(bad_codeblock)
+        assert result, result.output
 
 
 def test_good_codeblocks_have_no_lint_errors(lint_codeblock, good_codeblock):
-    assert lint_codeblock(good_codeblock), '"Good" codeblock caused lint error'
+    result = lint_codeblock(good_codeblock)
+    assert result, result.output
 
 
 def test_best_codeblocks_have_no_lint_errors(lint_codeblock, best_codeblock):
-    assert lint_codeblock(best_codeblock), '"Best" codeblock caused lint error'
+    result = lint_codeblock(best_codeblock)
+    assert result, result.output
