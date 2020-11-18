5db68ca8c96be0a454f4a6ca312886060bd89409
Joshua Cannon <joshua.cannon@ni.com>
2020-11-18 14:27:33 -0600

    Turn on pyflakes linting (#32)

--- tests/test_convention_doc/test_codeblocks.py
+++ tests/test_convention_doc/test_codeblocks.py
@@ -1,11 +1,26 @@
-import json
-
 import pytest
 
 
 @pytest.fixture
 def lint_codeblock(styleguide, tmp_path):
-    def run_linter(codeblock, *styleguide_args):
+    def run_linter(
+        codeblock,
+        *styleguide_args,
+        # We ignore unused import warnings as most of the codeblocks which produce this warning
+        # are just demonstrations of bad/good imports. Adding usage of the imports would add
+        # code to trivial examples, detracting from the interesting lines.
+        ignore_unused_imports=True,
+    ):
+
+        extend_ignore = [
+            # Undefined name. Defining all the names in each example would detract from the
+            # interesting lines.
+            "F821",
+        ]
+        if ignore_unused_imports:
+            extend_ignore.append("F401")
+        styleguide_args += ("--extend-ignore", ",".join(extend_ignore))
+
         test_file = tmp_path / "test.py"
         test_file.write_text(codeblock.contents, encoding="utf-8")
         return styleguide("lint", *styleguide_args, test_file)
@@ -23,7 +38,11 @@ def test_rule_codeblocks_documents_bad_good_best(codeblock):
 
 def test_bad_codeblocks_document_lint_errors(lint_codeblock, bad_codeblock):
     if bad_codeblock.rule.is_automatically_enforced:
-        result = lint_codeblock(bad_codeblock, "--format='%(code)s'")
+        result = lint_codeblock(
+            bad_codeblock,
+            "--format='%(code)s'",
+            ignore_unused_imports=("F401" not in bad_codeblock.rule.error_codes),
+        )
         assert not result, result.output
 
         error_codes = set(code.strip("'") for code in result.output.splitlines())
