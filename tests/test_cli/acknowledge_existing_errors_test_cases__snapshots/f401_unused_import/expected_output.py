b234c7b692f323ebe211227161f95a6a1fed4fb0
Matthew Shafer <matthew.shafer@ni.com>
2021-01-12 10:16:28 -0600

    move multiple erros on single line to single suppression statement

--- tests/test_cli/acknowledge_existing_errors_test_cases__snapshots/f401_unused_import/expected_output.py
+++ tests/test_cli/acknowledge_existing_errors_test_cases__snapshots/f401_unused_import/expected_output.py
@@ -3,4 +3,4 @@
 
 import os  # noqa F401: 'os' imported but unused - This suppression was auto-generated to allow focus on handling new errors
 from os import path  # noqa F401: 'os.path' imported but unused - This suppression was auto-generated to allow focus on handling new errors
-import pathlib, glob  # noqa F401: 'pathlib' imported but unused - This suppression was auto-generated to allow focus on handling new errors  # noqa E401: multiple imports on one line - This suppression was auto-generated to allow focus on handling new errors
+import pathlib, glob  # noqa F401: 'pathlib' imported but unused, E401: multiple imports on one line - This suppression was auto-generated to allow focus on handling new errors
--- tests/test_cli/acknowledge_existing_errors_test_cases__snapshots/f403_do_not_use_wildcard_import/expected_output.py
+++ tests/test_cli/acknowledge_existing_errors_test_cases__snapshots/f403_do_not_use_wildcard_import/expected_output.py
@@ -1,4 +1,4 @@
 """example of a python file with linter errors.
 """
 
-from os.path import *  # noqa F403: 'from os.path import *' used; unable to detect undefined names - This suppression was auto-generated to allow focus on handling new errors  # noqa F401: 'os.path.*' imported but unused - This suppression was auto-generated to allow focus on handling new errors
+from os.path import *  # noqa F403: 'from os.path import *' used; unable to detect undefined names, F401: 'os.path.*' imported but unused - This suppression was auto-generated to allow focus on handling new errors
