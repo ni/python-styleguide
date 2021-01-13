6e754548626fe8514dfcce9a534b129d30880193
Matthew Shafer <matthew.shafer@ni.com>
2021-01-13 09:08:24 -0600

    add method and param name tests

---
+++ tests/test_cli/acknowledge_existing_errors_test_cases__snapshots/n802_803_methods_and_params_with_bad_names/bad_input.py
@@ -0,0 +1,90 @@
+"""example of a python file with linter errors.
+"""
+
+import os
+
+os.listdir()
+
+
+def method_with_shadow_builtin(input):
+    """Shadow a builtin."""
+    return input
+
+
+def method_with_shadow_import(os):
+    """Shadow an import."""
+    return os
+
+
+def method_with_shadow_import_on_multiple_lines(
+    x,
+    y,
+    os,
+):
+    """Shadow an import."""
+    return os
+
+
+def method_with_unused_param(unused_input):
+    """Provide and unused param."""
+    return 5
+
+
+def method_with_parameters_on_multiple_lines(x, y):
+    """Provide parameters on multiple lines test case."""
+    return x + y
+
+
+def method_with_bad_names_on_signle_line(myBadlyNamedParam, my_other_Bad_name):
+    """Provide parameters with bad names on single line."""
+    return myBadlyNamedParam + my_other_Bad_name
+
+
+def method_with_bad_names_on_multiple_lines_1(
+    myBadlyNamedParam,
+):
+    """Provide parameters with bad names on multiple lines."""
+    return myBadlyNamedParam + 5
+
+
+def method_with_bad_names_on_multiple_lines_2(
+    myBadlyNamedParam,
+    my_other_Bad_name,
+):
+    """Provide parameters with bad names on multiple lines."""
+    return myBadlyNamedParam + my_other_Bad_name
+
+
+def method_withBadName_with_shadow(input):
+    """Shadow a builtin."""
+    return input
+
+
+def method_withBadName_with_unused_param(unused_input):
+    """Provide and unused param."""
+    return 5
+
+
+def method_withBadName_with_parameters_on_multiple_lines(x, y):
+    """Provide parameters on multiple lines test case."""
+    return x + y
+
+
+def method_withBadName_with_bad_params_on_signle_line(myBadlyNamedParam, my_other_Bad_name):
+    """Provide parameters with bad names on single line."""
+    return myBadlyNamedParam + my_other_Bad_name
+
+
+def method_withBadName_with_bad_params_on_multiple_lines_1(
+    myBadlyNamedParam,
+):
+    """Provide parameters with bad names on multiple lines."""
+    return myBadlyNamedParam + 5
+
+
+def method_withBadName_with_bad_params_on_multiple_lines_2(
+    myBadlyNamedParam,
+    my_other_Bad_name,
+):
+    """Provide parameters with bad names on multiple lines."""
+    return myBadlyNamedParam + my_other_Bad_name
