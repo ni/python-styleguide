c042134fd6fca5448f22431e22dc4de791ec2d0c
Matthew Shafer <matthew.shafer@ni.com>
2021-10-21 15:46:32 -0500

    move configuations files to single source

--- ni_python_styleguide/_lint.py
+++ ni_python_styleguide/_lint.py
@@ -1,7 +1,7 @@
 """Linting methods."""
-import pathlib
-
 import flake8.main.application
+
+from ni_python_styleguide import _config_consts
 
 
 def lint(qs_or_vs, exclude, app_import_names, format, extend_ignore, file_or_dir):
@@ -9,14 +9,14 @@ def lint(qs_or_vs, exclude, app_import_names, format, extend_ignore, file_or_dir
     app = flake8.main.application.Application()
     args = [
         qs_or_vs,
-        f"--config={(pathlib.Path(__file__).parent / 'config.ini').resolve()}",
+        f"--config={_config_consts.FLAKE8_CONFIG_FILE.resolve()}",
         f"--exclude={exclude}" if exclude else "",
         f"--format={format}" if format else "",
         f"--extend-ignore={extend_ignore}" if extend_ignore else "",
         # The only way to configure flake8-black's line length is through a pyproject.toml's
         # [tool.black] setting (which makes sense if you think about it)
         # So we need to give it one
-        f"--black-config={(pathlib.Path(__file__).parent / 'config.toml').resolve()}",
+        f"--black-config={_config_consts.BLACK_CONFIG_FILE.resolve()}",
         f"--application-import-names={app_import_names}",
         *file_or_dir,
     ]
