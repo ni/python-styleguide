bf45db8308c908cd30f1762e348ce87faac82840
Matthew Shafer <matthew.shafer@ni.com>
2021-10-21 17:57:28 -0500

    rename consts to constants to make pylint happy

--- ni_python_styleguide/_lint.py
+++ ni_python_styleguide/_lint.py
@@ -1,7 +1,7 @@
 """Linting methods."""
 import flake8.main.application
 
-from ni_python_styleguide import _config_consts
+from ni_python_styleguide import _config_constants
 
 
 def lint(qs_or_vs, exclude, app_import_names, format, extend_ignore, file_or_dir):
@@ -9,14 +9,14 @@ def lint(qs_or_vs, exclude, app_import_names, format, extend_ignore, file_or_dir
     app = flake8.main.application.Application()
     args = [
         qs_or_vs,
-        f"--config={_config_consts.FLAKE8_CONFIG_FILE.resolve()}",
+        f"--config={_config_constants.FLAKE8_CONFIG_FILE.resolve()}",
         f"--exclude={exclude}" if exclude else "",
         f"--format={format}" if format else "",
         f"--extend-ignore={extend_ignore}" if extend_ignore else "",
         # The only way to configure flake8-black's line length is through a pyproject.toml's
         # [tool.black] setting (which makes sense if you think about it)
         # So we need to give it one
-        f"--black-config={_config_consts.BLACK_CONFIG_FILE.resolve()}",
+        f"--black-config={_config_constants.BLACK_CONFIG_FILE.resolve()}",
         f"--application-import-names={app_import_names}",
         *file_or_dir,
     ]
