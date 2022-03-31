c444813f86e38481053fe0e9ef9ad7295273054e
Matthew Shafer <matthew.shafer@ni.com>
2022-03-31 18:21:16 -0500

    use the working tree dir

--- ni_python_styleguide/_utils/git.py
+++ ni_python_styleguide/_utils/git.py
@@ -6,11 +6,12 @@ from git import GitError, repo
 
 
 def find_repo_root(cwd: os.PathLike):
+    """Find the Git repo path or return None."""
     try:
         _repo = repo.Repo(str(cwd), search_parent_directories=True)
     except GitError:
         return None
-    return _repo.git_dir
+    return _repo.working_tree_dir
 
 
 def get_tracked_files(cwd: os.PathLike, *_, branch: str = "main", filter: str = "*.*"):
