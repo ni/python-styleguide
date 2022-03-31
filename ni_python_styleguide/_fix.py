1b4320960accfd1317ec3c9cb39ecf600ccb2008
Matthew Shafer <matthew.shafer@ni.com>
2022-03-31 16:23:59 -0500

    we can dog-food ourselves!

--- ni_python_styleguide/_fix.py
+++ ni_python_styleguide/_fix.py
@@ -2,14 +2,15 @@ import fileinput
 import logging
 import pathlib
 from collections import defaultdict
-from typing import Iterable, List
+from typing import Iterable
+from typing import List
 
 import isort
 
-from ni_python_styleguide import _acknowledge_existing_errors, _format
-from ni_python_styleguide._acknowledge_existing_errors import _lint_errors_parser
+from ni_python_styleguide import _acknowledge_existing_errors
+from ni_python_styleguide import _format
 from ni_python_styleguide import _utils
-
+from ni_python_styleguide._acknowledge_existing_errors import _lint_errors_parser
 
 _module_logger = logging.getLogger(__name__)
 
