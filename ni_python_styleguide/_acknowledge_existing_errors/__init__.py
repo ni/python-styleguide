c891708e4a447eb93e70c5f1e8068386263b4042
Matthew Shafer <matthew.shafer@ni.com>
2022-04-18 12:20:17 -0500

    refactor getting lint output to utils

--- ni_python_styleguide/_acknowledge_existing_errors/__init__.py
+++ ni_python_styleguide/_acknowledge_existing_errors/__init__.py
@@ -37,20 +37,9 @@ def _filter_suppresion_from_line(line: str):
 def _get_lint_errors_to_process(
     exclude, app_import_names, extend_ignore, file_or_dir, *_, excluded_errors=None
 ):
-    lint_errors = _lint.get_lint_output(
-        format=None,
-        qs_or_vs=None,
-        exclude=exclude,
-        app_import_names=app_import_names,
-        extend_ignore=extend_ignore,
-        file_or_dir=file_or_dir,
-    ).splitlines()
-    parsed_errors = map(_lint_errors_parser.parse, lint_errors)
-    parsed_errors = list(filter(None, parsed_errors))
     if excluded_errors is None:
         excluded_errors = EXCLUDED_ERRORS
-    lint_errors_to_process = [error for error in parsed_errors if error.code not in excluded_errors]
-    return lint_errors_to_process
+    return _utils.lint.get_lint_errors_to_process(exclude, app_import_names, extend_ignore, file_or_dir, excluded_errors)
 
 
 def acknowledge_lint_errors(
