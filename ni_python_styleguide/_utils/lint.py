298e288ef667135783ac089834f9b4dfaa550ab8
Matthew Shafer <matthew.shafer@ni.com>
2022-04-21 18:10:01 -0500

    format

--- ni_python_styleguide/_utils/lint.py
+++ ni_python_styleguide/_utils/lint.py
@@ -5,9 +5,7 @@ from collections import namedtuple
 from ni_python_styleguide import _lint
 
 
-def get_errors_to_process(
-    exclude, app_import_names, extend_ignore, file_or_dir, excluded_errors
-):
+def get_errors_to_process(exclude, app_import_names, extend_ignore, file_or_dir, excluded_errors):
     """Get lint errors to process."""
     lint_errors = _lint.get_lint_output(
         format=None,
