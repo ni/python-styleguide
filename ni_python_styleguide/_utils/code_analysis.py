0de5d0816201719bf9361cb11efa8feea35e12f9
Matthew Shafer <matthew.shafer@ni.com>
2022-05-09 11:26:44 -0500

    create method for for finding start and end of module level imports via ast

---
+++ ni_python_styleguide/_utils/code_analysis.py
@@ -0,0 +1,23 @@
+import ast
+import pathlib
+from typing import Tuple
+
+def find_import_region(file: pathlib.Path) -> Tuple[int, int]:
+    """Return the index of the first import line and the last import line that precedes any other code.
+
+    Note: will not handle try/except imports
+
+    file: pathlib.Path path to file to evaluate
+    Return: Tuple[int, int] the start and ending lines (1 based) during which the module level imports are
+    """
+    file_contents = file.read_text()
+    tree = ast.parse(file_contents)
+    end = start = 0
+    for node in tree.body: # only walk top level items
+        if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
+            continue
+        if isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
+            continue
+        end = node.lineno - 1
+        break
+    return start, end
