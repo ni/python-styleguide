e6c57467727c61d0ec7ac53d155fc2685bff7c06
Matthew Shafer <matthew.shafer@ni.com>
2022-05-09 11:30:57 -0500

    update doc strings

--- ni_python_styleguide/_utils/code_analysis.py
+++ ni_python_styleguide/_utils/code_analysis.py
@@ -4,12 +4,12 @@ from typing import Tuple
 
 
 def find_import_region(file: pathlib.Path) -> Tuple[int, int]:
-    """Return the index of the first import line and the last import line that precedes any other code.
+    """Return the index of the first last import line that precedes any other code.
 
     Note: will not handle try/except imports
 
     file: pathlib.Path path to file to evaluate
-    Return: Tuple[int, int] the start and ending lines (1 based) during which the module level imports are
+    Return: Tuple[int, int] the start and ending lines (0 based) of the module level imports
     """
     file_contents = file.read_text()
     tree = ast.parse(file_contents)
--- ni_python_styleguide/_utils/temp_file.py
+++ ni_python_styleguide/_utils/temp_file.py
@@ -5,6 +5,10 @@ from contextlib import contextmanager
 
 @contextmanager
 def multi_access_tempfile(suffix=".txt", delete=True):
+    """Tempfile that has been created and closed and will be deleted after this context.
+
+    Used to allow writing to a temp file multiple times.
+    """
     temp = tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
     temp.close()
     temp_path = pathlib.Path(temp.name)
