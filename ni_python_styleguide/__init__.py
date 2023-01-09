5ce1997c098cfdf3d37e87bb61306e693eb3aec6
Matthew Shafer 🐍 <matthew.shafer@ni.com>
2023-01-09 08:52:47 -0600

    add internal communication error class

--- ni_python_styleguide/__init__.py
+++ ni_python_styleguide/__init__.py
@@ -1,1 +1,7 @@
 """NI's internal and external style rules enforcement tool for Python."""
+
+
+class _Flake8Error(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+        self.code = args[0]
