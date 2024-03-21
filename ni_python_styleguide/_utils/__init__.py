40c26fe66c56e3612d348b321b36b664e4f737db
Matthew Shafer <matthew.shafer@ni.com>
2024-03-21 17:01:38 -0500

    make format produce something the linter is happy with

--- ni_python_styleguide/_utils/__init__.py
+++ ni_python_styleguide/_utils/__init__.py
@@ -1,8 +1,8 @@
+from . import _constants
 from . import code_analysis  # noqa: F401
 from . import diff  # noqa: F401
 from . import lint  # noqa: F401
 from . import string_helpers  # noqa: F401
 from . import temp_utils  # noqa: F401
-from . import _constants
 
 DEFAULT_ENCODING = _constants.DEFAULT_ENCODING
