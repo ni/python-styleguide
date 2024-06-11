734486f18affd2bebddb5b7668c84bc7ed94d131
mshafer-NI <matthew.shafer@ni.com>
2024-06-11 15:55:41 -0500

    make a failing test

--- tests/test_cli/test_fix.py
+++ tests/test_cli/test_fix.py
@@ -51,6 +51,9 @@ def test_given_bad_input__fix__produces_expected_output_aggressive(
     in_file = test_dir / "input.py"
     test_file = tmp_path / "input.py"
     shutil.copyfile(in_file, test_file)
+    test_loading_file = tmp_path / ".venv/file_with_non_unicode_chars.py"
+    test_loading_file.parent.mkdir(parents=True, exist_ok=True)
+    test_loading_file.write_text("Förward to victory!", encoding="ISO 8859-1")
 
     output = styleguide_command(command="fix", command_args=["--aggressive"])
 
