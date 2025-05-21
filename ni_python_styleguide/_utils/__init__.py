01b9d77e13fd728bcbec2e6efea0a49a142cc641
mshafer-NI <23644905+mshafer-NI@users.noreply.github.com>
2025-05-21 16:13:42 -0500

    adopt flake8-tidy-imports to enforce O.1.5 (#223)
    * make it show which rule is failing more clearly
    
    * add tool and update config and convention docs
    
    * add tool to lock file
    
    * add example import that doesn't use from
    
    * update own code to match
    
    * format
    
    * unformat
    
    * format
    
    * remove the exception note
    
    * add another ok example
    
    * try disabling mac testing for now
    
    * try byte comparison
    
    * ok, let's use an rstrip

--- ni_python_styleguide/_utils/__init__.py
+++ ni_python_styleguide/_utils/__init__.py
@@ -1,6 +1,6 @@
-from . import code_analysis  # noqa: F401
-from . import lint  # noqa: F401
-from . import string_helpers  # noqa: F401
-from . import temp_file  # noqa: F401
+from ni_python_styleguide._utils import code_analysis  # noqa: F401
+from ni_python_styleguide._utils import lint  # noqa: F401
+from ni_python_styleguide._utils import string_helpers  # noqa: F401
+from ni_python_styleguide._utils import temp_file  # noqa: F401
 
 DEFAULT_ENCODING = "UTF-8"
